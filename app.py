2267067f2bc9fb4ae457a98b4ba58a0cda4edac9
Natalka00 <nnogaj@gmail.com>
2023-11-02 17:26:11 +0100

    Added password requirements handling to app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = sqlite3.connect('database.db')
 
 @app.route("/")
 def index():
@@ -25,8 +24,37 @@ def register():
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
+        # Error: Empty fields
         if len(username) < 1 or len(password) < 1 or len(confirmation) < 1:
-            return error_message("Fields cannot be empty")
+            return error_message("Fields cannot be empty.")
+        
+        # Error: Existing username
+        db_connection = sqlite3.connect('database.db')
+        db_cursor = db_connection.cursor()
+        check_username = db_cursor.execute("SELECT username FROM users WHERE username = ?;", username)
+        if len(check_username.fetchone()) > 0:
+            db_connection.close()
+            return error_message("Username already exists.")
+        
+        # Error: Too short password
+        if len(password) < 8:
+            return error_message("Password must be at least 8 characters long.")
+
+        # Error: No digit in password
+        elif not any(character.isdigit() for character in password):
+            return error_message("Password must contain at least one digit.")
+
+        # Error: No uppercase letter
+        elif not any(character.isupper() for character in password):
+            return error_message("Password must contain at least one uppercase letter.")
+
+        # Error: No lowercase letter
+        elif not any(character.islower() for character in password):
+            return error_message("Password must contain at least one lowercase letter.")
         
     else:
         return render_template("register.html")
+    
+@app.route("/login", methods=["GET","POST"])
+def login():
+    return render_template("login.html")
